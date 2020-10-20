7c398d28001146bb43455fff5430d0f9b9918c01
shinichin <2252215674@qq.com>
2020-10-20 16:37:01 +0800

    1020--branch test

--- src/main/java/com/example/wn/utils/StringUtils.java
+++ src/main/java/com/example/wn/utils/StringUtils.java
@@ -2,7 +2,7 @@ package com.example.wn.utils;
 
 import java.util.Random;
 
-//生成指定长度随机字符串方法
+//生成指定长度随机字符串方法----
 public class StringUtils {
     public static String getRandomString(int length) {
         String base = "abcdefghijklmnopqrstuvwxyz0123456789";
